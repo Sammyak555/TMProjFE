79ae15a9b46aef5c57976cf514771c8a484fa65a
Sammyak555 <sammyak.deosale.1@gmail.com>
2023-04-25 11:46:17 +0530

    changes

--- src/Redux/Operation/action.js
+++ src/Redux/Operation/action.js
@@ -73,7 +73,7 @@ export const AddingProject=(data,id)=>(dispatch)=>{
     dispatch(addprojectreq())
     if(data&&id){
        console.log(data,id)
-    return axios.post(`https://tame-newt-garment.cyclic.app/project/${id}`,data)
+    return axios.post(`https://tame-blue-agouti-ring.cyclic.app/project/${id}`,data)
     .then((r)=>{
         dispatch(addprojectsucc(r.data))
     })
@@ -87,7 +87,7 @@ export const deletingProject=(data,id)=>(dispatch)=>{
     dispatch(deleteprojectreq())
     if(data&&id){
        console.log(data,id)
-    return axios.delete(`https://tame-newt-garment.cyclic.app/project/${id}/${data}`)
+    return axios.delete(`https://tame-blue-agouti-ring.cyclic.app/project/${id}/${data}`)
     .then((r)=>{
         dispatch(deleteprojectsucc(r.data))
     })
@@ -104,7 +104,7 @@ export const AddingTask=(userid,data)=>(dispatch)=>{
             taskName:data.issueDescription,
             AssignedTo:data.persontobeAssigned
         }
-    return axios.post(`https://tame-newt-garment.cyclic.app/task/${userid}/${data.project}`,payload)
+    return axios.post(`https://tame-blue-agouti-ring.cyclic.app/task/${userid}/${data.project}`,payload)
     .then((r)=>{
         dispatch(addtasksucc(r.data))
     })
@@ -121,7 +121,7 @@ export const updatingTask= (userid,projid,taskid,taskval)=>async(dispatch)=>{
        const payload ={ }
        payload[taskval]=true
         console.log(payload)
-    return axios.patch(`https://tame-newt-garment.cyclic.app/task/${userid}/${projid}/${taskid}`,payload)
+    return axios.patch(`https://tame-blue-agouti-ring.cyclic.app/task/${userid}/${projid}/${taskid}`,payload)
     .then((r)=>{
         dispatch(updatetasksucc(r.data))
     })
--- src/Redux/User/action.js
+++ src/Redux/User/action.js
@@ -24,7 +24,7 @@ export const getalluserfail=()=>{
 export const getUserDetails= async (dispatch) =>{
 
     dispatch(getuserreq())
-    return await axios.get(`https://tame-newt-garment.cyclic.app/users/641d5b0af383b010e8296e5d`)
+    return await axios.get(`https://tame-blue-agouti-ring.cyclic.app/users/641d5b0af383b010e8296e5d`)
     .then((r)=>{
         dispatch(getusersucc(r.data))
     })
@@ -36,7 +36,7 @@ export const getUserDetails= async (dispatch) =>{
 export const getallUserDetails= async (dispatch) =>{
 
     dispatch(getalluserreq())
-    return await axios.get(`https://tame-newt-garment.cyclic.app/users/`)
+    return await axios.get(`https://tame-blue-agouti-ring.cyclic.app/users/`)
     .then((r)=>{
         dispatch(getallusersucc(r.data))
     })
